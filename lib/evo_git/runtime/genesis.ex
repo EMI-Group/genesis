5281b29f0022e780fae5640bdff321b900574487
Bill Huang <bill.huang2001@gmail.com>
2026-01-29 15:54:30 +0800

    Move to separate module

---
+++ lib/evo_git/runtime/genesis.ex
@@ -0,0 +1,7 @@
+defmodule EvoGit.Runtime.Genesis do
+  @moduledoc "Stage 1: Creation Phase"
+  def run(root_prompt) do
+    # 1. Initialize Root
+    # 2. Recursive Expansion
+  end
+end
---
+++ lib/evo_git/runtime/optimization.ex
@@ -0,0 +1,6 @@
+defmodule EvoGit.Runtime.Optimization do
+  @moduledoc "Stage 2: Evolutionary Loop"
+  def run(objective) do
+    # Diagnosis -> Dispatch -> Evolution -> Merge
+  end
+end
