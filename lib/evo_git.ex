f3aface1346a23dcce168eb8db79005d96cb0b30
Bill Huang <bill.huang2001@gmail.com>
2026-03-10 17:50:46 +0800

    Add systemd-run sandbox

--- lib/evo_git.ex
+++ lib/evo_git.ex
@@ -4,15 +4,34 @@ defmodule EvoGit do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> EvoGit.hello()
-      :world
-
+  Generates systemd-run arguments to execute a command inside a cheap sandbox.
   """
-  def hello do
-    :world
+  def sandbox_args(cwd, executable, args \\ []) do
+    [
+      "--user",
+      "--wait",
+      "--pipe",
+      "-p",
+      "ProtectSystem=strict",
+      "-p",
+      "ProtectHome=read-only",
+      "-p",
+      "ReadWritePaths=#{cwd}",
+      "-p",
+      "PrivateTmp=yes",
+      "-p",
+      "NoNewPrivileges=yes",
+      "-p",
+      "SystemCallArchitectures=native",
+      "-p",
+      "SystemCallErrorNumber=EPERM",
+      "-p",
+      "SystemCallFilter=~ @module @keyring @raw-io @reboot @mount @swap @clock @cpu-emulation @debug @obsolete @privileged",
+      "-p",
+      "CPUQuota=400%",
+      "-p",
+      "MemoryMax=16G",
+      executable
+    ] ++ args
   end
 end
