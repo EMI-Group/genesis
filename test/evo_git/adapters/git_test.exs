9f799c2d31f400e245bcc5cf085ea48cec0c69e9
Bill Huang <bill.huang2001@gmail.com>
2026-04-08 11:42:21 +0800

    Add helper function for inspecting the git history

--- test/evo_git/adapters/git_test.exs
+++ test/evo_git/adapters/git_test.exs
@@ -54,4 +54,53 @@ defmodule EvoGit.Adapters.GitTest do
     {:ok, ignored} = Git.check_ignore(tmp_dir, ["normal.txt", "other.txt"])
     assert ignored == []
   end
+
+  test "log and file_history", %{tmp_dir: tmp_dir} do
+    File.write!(Path.join(tmp_dir, "test.txt"), "initial content")
+    Git.add(tmp_dir, "test.txt")
+    Git.commit(tmp_dir, "Initial commit")
+
+    File.write!(Path.join(tmp_dir, "test.txt"), "updated content")
+    Git.add(tmp_dir, "test.txt")
+    Git.commit(tmp_dir, "Updated commit")
+
+    assert {:ok, log_output} = Git.log(tmp_dir, ["--oneline"])
+    assert String.contains?(log_output, "Updated commit")
+    assert String.contains?(log_output, "Initial commit")
+
+    assert {:ok, history} = Git.file_history(tmp_dir, "test.txt", ["--oneline"])
+    assert String.contains?(history, "Updated commit")
+    assert String.contains?(history, "Initial commit")
+  end
+
+  test "show object", %{tmp_dir: tmp_dir} do
+    File.write!(Path.join(tmp_dir, "test.txt"), "initial content")
+    Git.add(tmp_dir, "test.txt")
+    Git.commit(tmp_dir, "Initial commit")
+
+    assert {:ok, show_output} = Git.show(tmp_dir, "HEAD:test.txt")
+    assert show_output == "initial content"
+  end
+
+  test "diff and file_diff", %{tmp_dir: tmp_dir} do
+    File.write!(Path.join(tmp_dir, "test.txt"), "initial content\n")
+    Git.add(tmp_dir, "test.txt")
+    Git.commit(tmp_dir, "Initial commit")
+
+    {:ok, commit_a} = Git.rev_parse(tmp_dir, "HEAD")
+
+    File.write!(Path.join(tmp_dir, "test.txt"), "updated content\n")
+    Git.add(tmp_dir, "test.txt")
+    Git.commit(tmp_dir, "Updated commit")
+
+    {:ok, commit_b} = Git.rev_parse(tmp_dir, "HEAD")
+
+    assert {:ok, diff_output} = Git.diff(tmp_dir, commit_a, commit_b)
+    assert String.contains?(diff_output, "-initial content")
+    assert String.contains?(diff_output, "+updated content")
+
+    assert {:ok, file_diff_output} = Git.file_diff(tmp_dir, "test.txt", commit_a, commit_b)
+    assert String.contains?(file_diff_output, "-initial content")
+    assert String.contains?(file_diff_output, "+updated content")
+  end
 end
