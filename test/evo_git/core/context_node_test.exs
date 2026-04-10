15a634b93195d43e073aafadfa4553db83fbe0ef
Bill Huang <bill.huang2001@gmail.com>
2026-04-10 22:38:31 +0800

    Refactor the build context api

--- test/evo_git/core/context_node_test.exs
+++ test/evo_git/core/context_node_test.exs
@@ -11,7 +11,7 @@ defmodule EvoGit.Core.ContextNodeTest do
     File.write!(Path.join(tmp_dir, "lib/my_module.ex"), "defmodule MyModule do end")
 
     target_path = "lib/my_module.ex"
-    hierarchy = ContextNode.hierarchy_nodes(target_path, tmp_dir)
+    hierarchy = ContextNode.hierarchy_nodes!(target_path, tmp_dir)
 
     assert length(hierarchy) == 3
     assert Enum.at(hierarchy, 0).path == "."
@@ -23,26 +23,26 @@ defmodule EvoGit.Core.ContextNodeTest do
     File.mkdir_p!(Path.join(tmp_dir, "nested/deep"))
     target_path = "nested/deep/file.txt"
 
-    hierarchy = ContextNode.hierarchy_nodes(target_path, tmp_dir)
+    hierarchy = ContextNode.hierarchy_nodes!(target_path, tmp_dir)
 
     assert length(hierarchy) == 4
   end
 
   test "hierarchy_nodes/2 raises on absolute path", %{tmp_dir: tmp_dir} do
     assert_raise ArgumentError, fn ->
-      ContextNode.hierarchy_nodes("/etc/passwd", tmp_dir)
+      ContextNode.hierarchy_nodes!("/etc/passwd", tmp_dir)
     end
   end
 
   test "hierarchy_nodes/2 with dot relative path", %{tmp_dir: tmp_dir} do
-    hierarchy = ContextNode.hierarchy_nodes(".", tmp_dir)
+    hierarchy = ContextNode.hierarchy_nodes!(".", tmp_dir)
     assert length(hierarchy) == 1
     assert Enum.at(hierarchy, 0).path == "."
   end
 
   test "hierarchy_nodes/2 with path outside root returns ArgumentError", %{tmp_dir: tmp_dir} do
     assert_raise ArgumentError, fn ->
-      ContextNode.hierarchy_nodes("../outside", tmp_dir)
+      ContextNode.hierarchy_nodes!("../outside", tmp_dir)
     end
   end
 end
