4ecb59178e4d4f2f8eba7b7c8bbfd25cb87b6bf3
Bill Huang <bill.huang2001@gmail.com>
2026-04-15 13:57:48 +0800

    Set up the standard configuration files

---
+++ config/config.exs
@@ -0,0 +1,8 @@
+import Config
+
+# Configures the application
+# config :evo_git, key: :value
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
