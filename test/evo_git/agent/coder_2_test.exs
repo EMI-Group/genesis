4904bf9aea622688aba6efa096c100f4366c255c
Bill Huang <bill.huang2001@gmail.com>
2026-04-15 14:38:10 +0800

    Rename test filename

--- test/evo_git/agent/coder_2_test.exs
+++ test/evo_git/agent/coder_2_test.exs
@@ -12,10 +12,10 @@ defmodule EvoGit.Agent.CoderTest2 do
     end
   end
 
-  test "build_dynamic_context with node_path == repo_path", %{tmp_dir: tmp_dir} do
+  test "build_dynamic_context with node_path at repo root", %{tmp_dir: tmp_dir} do
     File.write!(Path.join(tmp_dir, "CONTEXT.md"), "Root context")
 
-    context = DummyAgent.test_build_dynamic_context(tmp_dir, tmp_dir)
+    context = DummyAgent.test_build_dynamic_context(tmp_dir, ".")
 
     assert context =~ "Root context"
     assert String.length(context) < 1000
