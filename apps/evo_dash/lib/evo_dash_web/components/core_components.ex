6b2832e7339b07189e9e918768599a4aa7be27e1
Bill Huang <bill.huang2001@gmail.com>
2026-06-01 00:09:04 +0800

    Uncomment gettext calls in translate_error/1
    Co-authored-by: EvoGit <noreply@evogit.ai>

--- apps/evo_dash/lib/evo_dash_web/components/core_components.ex
+++ apps/evo_dash/lib/evo_dash_web/components/core_components.ex
@@ -449,18 +449,11 @@ defmodule EvoDashWeb.CoreComponents do
   Translates an error message using gettext.
   """
   def translate_error({msg, opts}) do
-    # You can make use of gettext to translate error messages by
-    # uncommenting and adjusting the following code:
-
-    # if count = opts[:count] do
-    #   Gettext.dngettext(EvoDashWeb.Gettext, "errors", msg, msg, count, opts)
-    # else
-    #   Gettext.dgettext(EvoDashWeb.Gettext, "errors", msg, opts)
-    # end
-
-    Enum.reduce(opts, msg, fn {key, value}, acc ->
-      String.replace(acc, "%{#{key}}", fn _ -> to_string(value) end)
-    end)
+    if count = opts[:count] do
+      Gettext.dngettext(EvoDashWeb.Gettext, "errors", msg, msg, count, opts)
+    else
+      Gettext.dgettext(EvoDashWeb.Gettext, "errors", msg, opts)
+    end
   end
 
   @doc """
