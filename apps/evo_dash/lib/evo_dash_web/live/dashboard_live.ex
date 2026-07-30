b86ae86e97eda77fd644ac710f6dfed3e2ea1744
Bill Huang <bill.huang2001@gmail.com>
2026-07-31 02:52:10 +0800

    Extract project creation/opening handlers to ProjectFlow module
    Co-authored-by: Genesis <noreply@evogit.ai>

--- apps/evo_dash/lib/evo_dash_web/live/dashboard_live.ex
+++ apps/evo_dash/lib/evo_dash_web/live/dashboard_live.ex
@@ -8,7 +8,7 @@ defmodule EvoDashWeb.DashboardLive do
   """
   use EvoDashWeb, :live_view
   alias EvoGit.TaskRegistry
-  alias EvoDashWeb.DashboardLive.{StatePersistence, Project, Assigns}
+  alias EvoDashWeb.DashboardLive.{StatePersistence, Project, Assigns, ProjectFlow}
   alias EvoGit.Core.ForeignRepo
   alias EvoGit.Platform
   alias EvoGit.ProjectConfig
@@ -484,21 +484,13 @@ defmodule EvoDashWeb.DashboardLive do
   # --- Project Management Events ---
 
   @impl true
-  def handle_event("toggle_open_project_form", _params, socket) do
-    {:noreply,
-     assign(socket,
-       show_open_project_form: !socket.assigns.show_open_project_form,
-       show_new_project_form: false
-     )}
+  def handle_event("toggle_open_project_form", params, socket) do
+    ProjectFlow.toggle_open_project_form(socket, params)
   end
 
   @impl true
-  def handle_event("toggle_new_project_form", _params, socket) do
-    {:noreply,
-     assign(socket,
-       show_new_project_form: !socket.assigns.show_new_project_form,
-       show_open_project_form: false
-     )}
+  def handle_event("toggle_new_project_form", params, socket) do
+    ProjectFlow.toggle_new_project_form(socket, params)
   end
 
   @impl true
@@ -507,86 +499,18 @@ defmodule EvoDashWeb.DashboardLive do
   end
 
   @impl true
-  def handle_event("create_project", %{"location" => location, "name" => name}, socket) do
-    location = Path.expand(location)
-
-    cond do
-      not File.dir?(location) ->
-        {:noreply,
-         put_flash(
-           socket,
-           :error,
-           gettext("Parent directory does not exist: %{path}", path: location)
-         )}
-
-      true ->
-        case Project.validate_project_name(name) do
-          {:error, :invalid_name} ->
-            {:noreply, put_flash(socket, :error, gettext("Invalid project name"))}
-
-          {:ok, sanitized} ->
-            full_path = Path.join(location, sanitized)
-            File.mkdir!(full_path)
-
-            TaskRegistry.add_recent_project(full_path, sanitized)
-            recent_projects = TaskRegistry.list_recent_projects()
-
-            socket =
-              socket
-              |> assign(:recent_projects, recent_projects)
-              |> assign(:show_new_project_form, false)
-              |> put_flash(:info, gettext("Project created: %{path}", path: full_path))
-
-            {:noreply, push_patch(socket, to: ~p"/?project=#{URI.encode(full_path)}")}
-        end
-    end
+  def handle_event("create_project", params, socket) do
+    ProjectFlow.create_project(socket, params)
   end
 
   @impl true
-  def handle_event("open_project", %{"path" => path}, socket) do
-    expanded = Path.expand(path)
-
-    if File.dir?(expanded) do
-      TaskRegistry.add_recent_project(expanded, Path.basename(expanded))
-      recent_projects = TaskRegistry.list_recent_projects()
-
-      socket =
-        socket
-        |> assign(:recent_projects, recent_projects)
-        |> assign(:show_open_project_form, false)
-
-      # Push URL params to persist project across navigation
-      {:noreply, push_patch(socket, to: ~p"/?project=#{URI.encode(expanded)}")}
-    else
-      {:noreply,
-       socket
-       |> put_flash(
-         :error,
-         gettext(
-           "Directory does not exist: %{path}. Create a new project instead?",
-           path: path
-         )
-       )}
-    end
+  def handle_event("open_project", params, socket) do
+    ProjectFlow.open_project(socket, params)
   end
 
   @impl true
-  def handle_event("select_project", %{"path" => path}, socket) do
-    expanded = Path.expand(path)
-
-    if File.dir?(expanded) do
-      TaskRegistry.add_recent_project(expanded, Path.basename(expanded))
-      recent_projects = TaskRegistry.list_recent_projects()
-
-      socket =
-        socket
-        |> assign(:recent_projects, recent_projects)
-
-      {:noreply, push_patch(socket, to: ~p"/?project=#{URI.encode(expanded)}")}
-    else
-      {:noreply,
-       put_flash(socket, :error, gettext("Directory does not exist: %{path}", path: path))}
-    end
+  def handle_event("select_project", params, socket) do
+    ProjectFlow.select_project(socket, params)
   end
 
   # --- Task Form Events ---
